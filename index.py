597c745aee359d85d9ee3b0fddc69a8a68b585e0
shamaz332 <shamazsaeed332@gmail.com>
2021-02-17 00:12:22 +0500

    Strings

--- index.py
+++ index.py
@@ -11,15 +11,32 @@
 #Age calculation
 #=============================
 
+
 # ageYear = input("Enter your Birth year "  )
 # age = 2020 - int(ageYear)
 # print("This is your age" ,age)
+
 
 #=============================
 #Calculaton
 #=============================
 
-valueFirst = float(input("Enter Value"))
-valueSecond = float(input("Enter Value"))
-sum  = valueSecond + valueFirst
-print(sum)
+
+# valueFirst = float(input("Enter Value"))
+# valueSecond = float(input("Enter Value"))
+# sum  = valueSecond + valueFirst
+# print(sum)
+
+
+#=============================
+#Playing with Strings
+#=============================
+
+
+# presence = "Hello world"
+# print("Hello" in presence)
+
+
+#=============================
+#Calculaton
+#=============================
