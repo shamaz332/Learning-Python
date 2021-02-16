9f58889747afa575f53e80f351f1f520f079947c
shamaz332 <shamazsaeed332@gmail.com>
2021-02-17 00:02:18 +0500

    Age calcu and simple add operation

--- index.py
+++ index.py
@@ -1,5 +1,25 @@
 #=============================
 #Taking Input
 #=============================
-name = input("Enter name")
-print("Hello",name)
+
+
+# name = input("Enter name")
+# print("Hello",name)
+
+
+#=============================
+#Age calculation
+#=============================
+
+# ageYear = input("Enter your Birth year "  )
+# age = 2020 - int(ageYear)
+# print("This is your age" ,age)
+
+#=============================
+#Calculaton
+#=============================
+
+valueFirst = float(input("Enter Value"))
+valueSecond = float(input("Enter Value"))
+sum  = valueSecond + valueFirst
+print(sum)
